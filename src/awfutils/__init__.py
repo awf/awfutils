f7c56066b564b4c114543a5962749380748a44b5
Andrew Fitzgibbon <awf@fitzgibbon.ie>
2025-08-30 16:51:05 +0100

    add def dict_to_simple_namespace(d):

--- src/awfutils/__init__.py
+++ src/awfutils/__init__.py
@@ -16,4 +16,4 @@ from .print_utils import fn_name, class_name
 from .typecheck import get_ast_for_function, typecheck
 from .ml_collections_tools import update_from_argv
 from .MkSweep import MkSweep
-from .misc import import_from_file
+from .misc import import_from_file, dict_to_simple_namespace
